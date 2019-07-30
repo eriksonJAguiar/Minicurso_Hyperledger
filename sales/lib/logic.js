648eaf9d6129c46065fa9932d56af897ad3dd9ff
eriksonJAguiar <erjulioaguiar@gmail.com>
2019-07-30 14:00:03 -0300

    projeto 1

---
+++ sales/lib/logic.js
@@ -0,0 +1,22 @@
+/**
+ * @param {org.tradesalecar.Sale} Sale
+ * @transaction
+ */
+
+ async function sale(contract){
+     const car = contract.car;
+     const price = contract.saleContract.price
+     const terms = contract.saleContract.saleId
+     const seller = contract.saleContract.seller
+     const buyer = contract.saleContract.buyer
+     seller.amount += price;
+     buyer.amount -= price;
+
+     //update seller amount
+     const sellerRegistry = await getParticipantRegistry('org.tradesalecar.Seller');
+     await sellerRegistry.update(seller);
+
+     //Update buyer amount
+     const buyerRegistry = await getParticipantRegistry('org.tradesalecar.Buyer');
+     await sellerRegistry.update(buyer);
+ }
